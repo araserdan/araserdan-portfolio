9d33aa3bec168768d11b31a5760e4d22266b6c7e
araserdan <ara.serdan18@gmail.com>
2023-09-24 17:00:01 +0800

    added pages, installed bootstrap react

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Experiences from './pages/Experiences';
+
+const Home = () => {
+  return (
+    <div>
+        <p>Home</p>
+        <Nav />
+        
+    </div>
+  );
+};
+
+export default Home;
